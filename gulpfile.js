bdaf018186af16211a9fb0bf84e79e8f404a35b3
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-06-08 20:40:20 +0200

    Added js minification to all folder levels.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 var gulp = require('gulp'),
-uglify = require('gulp-uglify'),
-htmlmin = require('gulp-html-minifier');
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-html-minifier');
 
 // Minify Javascript
-gulp.task('minify-js', function(){
-   gulp.src('js/*.js')
-   .pipe(uglify())
-   .pipe(gulp.dest('dist/js/'));
+gulp.task('minify-js', function () {
+    gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js/')),
+    gulp.src('views/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/views/js/'))
 });
 
 // Minify HTML
 gulp.task('minify-html', function () {
-  gulp.src('./*.html')
+    gulp.src('./*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./dist')),
-     gulp.src('views/*.html')
+    gulp.src('views/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./dist/views'))
 });
 
 
-gulp.task('default', ['minify-js','minify-html']);
+gulp.task('default', ['minify-js', 'minify-html']);
